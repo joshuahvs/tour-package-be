5e44f10b4002f95299323f443994df6a42da084a
vito1012buss <thebestvitodiaries@gmail.com>
2025-10-25 13:03:23 +0700

    init

---
+++ tour-package-2306165540-be/src/test/java/apap/ti/_5/tour_package_2306165540_be/TourPackage2306165540BeApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti._5.tour_package_2306165540_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourPackage2306165540BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
