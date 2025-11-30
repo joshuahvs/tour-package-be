fbd2627afaffd63f5d236d9965cb08ea54a99646
Joshua Hans Vito Soehendra <thebestvitodiaries@gmail.com>
2025-11-30 15:50:32 +0700

    try deploy

--- src/main/java/apap/ti/_5/tour_package_2306165540_be/config/CorsConfig.java
+++ src/main/java/apap/ti/_5/tour_package_2306165540_be/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package apap.ti._5.tour_package_2306165540_be.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,22 +8,51 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+
+    @Value("${CORS_ALLOWED_ORIGINS}")
+    private String allowedOrigins;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry
-                    .addMapping("/**")
-                    .allowedOrigins(
-                        "http://localhost:5173"
-                    )
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                    .allowedHeaders("*")
-                    .allowCredentials(true)
-                    .exposedHeaders("Authorization");
+                String[] origins = allowedOrigins.split(",");
+
+                registry.addMapping("/**")
+                        .allowedOrigins(origins)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true)
+                        .exposedHeaders("Authorization");
             }
         };
     }
 }
+
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// @Configuration
+// public class CorsConfig {
+//     @Bean
+//     public WebMvcConfigurer corsConfigurer() {
+//         return new WebMvcConfigurer() {
+//             @Override
+//             public void addCorsMappings(CorsRegistry registry) {
+//                 registry
+//                     .addMapping("/**")
+//                     .allowedOrigins(
+//                         "http://localhost:5173"
+//                     )
+//                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                     .allowedHeaders("*")
+//                     .allowCredentials(true)
+//                     .exposedHeaders("Authorization");
+//             }
+//         };
+//     }
+// }
 
